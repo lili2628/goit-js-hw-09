82ded6e2b648abe6df445e35ab63c4ea650546d0
lili2628 <113315152+lili2628@users.noreply.github.com>
2023-02-16 11:31:55 +0000

    Deploying to gh-pages from @ lili2628/goit-js-hw-09@affc923c836cd175e4a425d35ce3cd9536817035 🚀

--- 01-color-switcher.3cd5fcd4.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.body;function a(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;d.style.backgroundColor=t}t.addEventListener("click",(function(t){timerId=setInterval(a,1e3),t.target.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(e){clearInterval(timerId),e.target.disabled=!0,t.disabled=!1})),e.disabled=!0;
-//# sourceMappingURL=01-color-switcher.3cd5fcd4.js.map
